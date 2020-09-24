8b02d14a4edfae3c6819ddb6552f5456289ddaf7
Michael Sanderson <jawm17@gmail.com>
2020-09-23 18:19:21 -0700

    React router

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Router, Route, Switch } from "react-router-dom";
+import history from './history';
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router history={history}>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        {/* <Route path="/">
+        <NoMatchPage />
+      </Route> */}
+      </Switch>
+    </Router>
   );
 }
 
